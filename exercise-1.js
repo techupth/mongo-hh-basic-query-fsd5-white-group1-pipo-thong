9a2fcd0def098e7b93ace96a37c6824962490ae5
PIPOAUMPHOL <aumpholllo@gmail.com>
2024-01-10 20:29:21 +0700

    feat:add completed exercise

---
+++ exercise-1.js
@@ -0,0 +1,9 @@
+{
+  _id: ObjectId('659e86c40dbc780cfd3686af'),
+  size: 'medium',
+  total_price: 4627.41,
+  quantity: 12,
+  customer_name: 'Cherlyn',
+  credit_card_type: 'mastercard',
+  created_at: 2021-06-27T23:57:12.000Z
+}
